dafa09a6266283b0916e80f34344aeb1d919c183
xxj15 <seojini00@naver.com>
2025-05-06 01:03:27 +0900

    원시타입과 리터럴 타입

---
+++ section02/src/chatper1.ts
@@ -0,0 +1,30 @@
+// number
+let num1: number = 123;
+let num2: number = -123;
+let num3: number = 0.123;
+let num4: number = -0.123;
+let num5: number = Infinity;
+let num6: number = -Infinity;
+let num7: number = NaN;
+
+// string
+let str1: string = "hello";
+let str2: string = "hello";
+let str3: string = `hello`;
+let str4: string = `hello ${str1}`;
+
+// boolean
+let bool1: boolean = true;
+let bool2: boolean = false;
+
+// null
+let null1: null = null;
+
+// undefined 타입
+let unde1: undefined = undefined;
+
+// 리터럴 타입
+let numA: 10 = 10;
+let strA: "hello" = "hello";
+let boolA: true = true;
+let boolB: false = false;
