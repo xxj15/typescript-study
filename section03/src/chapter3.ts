ffe6e4ef1481d2c2922705ce3a6f05bca7484ac9
xxj15 <seojini00@naver.com>
2025-05-07 20:07:06 +0900

    객체 타입의 호환성

--- section03/src/chapter3.ts
+++ section03/src/chapter3.ts
@@ -34,3 +34,34 @@ let dog: Dog = {
 
 animal = dog; // OK
 // dog = animal; 오류 발생
+
+// 초과 프러퍼티 검사
+type Book = {
+  name: string;
+  price: number;
+};
+
+type ProgrammingBook = {
+  name: string;
+  price: number;
+  skill: string;
+};
+
+let book: Book;
+let programmingBook: ProgrammingBook = {
+  name: "한 입 크기로 잘라먹는 리액트",
+  price: 33000,
+  skill: "reactjs",
+};
+
+let book2: Book = {
+  // 오류 발생
+  name: "한 입 크기로 잘라먹는 리액트",
+  price: 33000,
+  // skill: "reactjs", 오류발생 - subtype 넣는건 업캐스팅인데 ??
+};
+
+// 초과 프로퍼티 검사 : 변수를 초기화할 때 초기화하는 값으로 객체 리터럴을 사용하면 발동하는 검사
+
+let book3: Book = programmingBook; // 이렇게 하면 사용 가능 (초기화할 때 객체 리터럴을 사용한건 아니니까)
+// 함수도 마찬가지로 초과 프로퍼티 검사 진행 - 위랑 마찬가지 과정 진행해야함
